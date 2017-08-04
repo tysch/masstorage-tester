0b1779ad41ec8d511c288d4ba23a9946728cdbde
tysch <adcmp600@gmail.com>
2017-08-05 00:51:23 +0300

    Completed filling function

--- main.c
+++ main.c
@@ -4,11 +4,18 @@
 #include <time.h>
 
 
+uint32_t state0;
+uint32_t state1;
+uint32_t state2;
+uint32_t state3;
 
-static uint32_t state0 = 1;
-static uint32_t state1 = 0;
-static uint32_t state2 = 0;
-static uint32_t state3 = 0;
+void reseed(uint64_t seed)
+{
+	state0 = seed & 0xFFFFFFFFU;
+	state1 = seed >> 32;
+	state2 = 0;
+	state3 = 0;
+}
 
 uint32_t xorshift128()
 {
@@ -35,7 +42,6 @@ uint64_t tobytes(char * x)
 			bytes *= 10;
 			bytes += (x[i] - 48);
 		}
-		if(x[i] == ' ') continue;
 		if((x[i] > 'A') && (x[i] < 'z'))
 		{
 			switch (x[i])
@@ -65,24 +71,47 @@ uint64_t tobytes(char * x)
 	return bytes;
 }
 
-
-int main(void)
-{	
-	time_t start = time(0);
-	uint32_t t;
-	uint32_t j = 0;
-	FILE* destFile;
-	destFile = fopen("test.dat", "wb");
+void fill(uint64_t bytes)
+{
+	int n1gfiles = bytes / (1024*1024*1024);
+	int fractsize = bytes % (1024*1024*1024);
+	char filename[10];
 	uint32_t buf[1024];
-	for(int i = 0; i < 1024*1024;i++)
+	for(int i = 0; i < n1gfiles; i++) //writing 1GiB files
+	{
+		sprintf(filename, "%i.dat",i);
+		FILE* destfile = fopen(filename, "wb");
+		for(int i = 0; i < 1024*1024;i++)
+		{
+			for(int j = 0; j < 256; j++)
+			{
+				buf[j] = xorshift128();
+			}
+			fwrite(buf, 4, 256, destfile);
+		}
+		fclose(destfile);
+	}
+	//writing remaining data
+	int nblocks1k = fractsize / 1024;
+	FILE* destfile = fopen("fract.dat", "wb");
+	for(int i = 0; i < nblocks1k;i++)
 	{
 		for(int j = 0; j < 256; j++)
 		{
 			buf[j] = xorshift128();
 		}
-		fwrite(buf, 4, 256, destFile);
+		fwrite(buf, 4, 256, destfile);
 	}
-	printf("%llu\n",tobytes("1000m"));
+	fclose(destfile);
+}
+
+
+
+int main(void)
+{	
+	reseed(1);
+	fill(tobytes("1200M"));
+	time_t start = time(0);
 	printf("elapsed %.3f\n", (time(0) - start)/1.0);
 	printf("%u \n", state0);	printf("%u \n", state1);	printf("%u \n", state2);	printf("%u \n", state3);
 	return 0;
